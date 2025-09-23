6ab5f446ff94dece6ddd9cc959090139bb245da2
boris honck <162339176+mauricedonck@users.noreply.github.com>
2025-09-23 09:52:22 +0200

    numer vak

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // JavaScript Document
-var name=""; 
+var name="";
+let numberToGuess = null;
+
 function savename(){
 	name=document.getElementById("nameuser").value;
 	console.log(name);
-	
-	
 } 
+
+
+function startGame() {
+	numberToGuess = Math.floor(Math.random() * max=10 min=1);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	startGame();
+});
+
+function inputnumber(){
+	let number=document.getElementById("numberinput").value;
+	console.log(number)
+}
